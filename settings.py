926856c3119220f394c2e2ffff58dc1e2257be8b
ethanlvshimself <160270531+ethanlvshimself@users.noreply.github.com>
2024-02-19 21:58:11 -0800

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,15 @@
+# This file was created by: Ethan Suriaga
+TITLE = "my fun game"
+
+FPS = 30
+
+WIDTH = 1024
+HEIGHT = 768
+TILESIZE = 32
+
+SKYBLUE = (111, 186, 234)
+
+LIGHTGREY = (100,100,100)
+GREEN = (0, 255, 0)
+YELLOW = (255, 255, 0)
+BGCOLOR = SKYBLUE
