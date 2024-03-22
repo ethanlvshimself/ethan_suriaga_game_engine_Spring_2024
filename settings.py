4cd8de5c67dd0692f3651ebaaf5d4ee3864ac9b3
ethanlvshimself <160270531+ethanlvshimself@users.noreply.github.com>
2024-03-22 09:20:51 -0700

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,15 +1,30 @@
 # This file was created by: Ethan Suriaga
-TITLE = "my fun game"
+TITLE = "Don't Leave."
 
-FPS = 30
+FPS = 32
 
 WIDTH = 1024
 HEIGHT = 768
 TILESIZE = 32
+PLAYER_SPEED = 250
+MOB_SPEED = 200
+
 
 SKYBLUE = (111, 186, 234)
 
 LIGHTGREY = (100,100,100)
 GREEN = (0, 255, 0)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
 YELLOW = (255, 255, 0)
-BGCOLOR = SKYBLUE
+BLUE = (0, 0, 255)
+DARKRED = (102, 0, 0)
+WHITE = (255,255,255)
+BGCOLOR = BLACK
+
+# Timer settings
+COUNTDOWN_DURATION = 45  # Countdown duration in seconds
+
+
+
+
